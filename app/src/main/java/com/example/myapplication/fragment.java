ef975d1400cfdf8d29d7d8f31ce9a76286cac353
kimpan <kim.pan0519@gmail.com>
2020-09-17 18:02:39 +0800

    update

--- app/src/main/java/com/example/myapplication/fragment.java
+++ app/src/main/java/com/example/myapplication/fragment.java
@@ -16,29 +16,33 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class fragment extends AppCompatActivity {
-    TextView title,sender,address,context,date;
-    String[] act_title,act_address,act_context,act_sender;
+    TextView title, sender, address, context, date;
+    String[] act_title, act_address, act_context, act_sender;
     Date[] act_date;
     Long[] postid;
+    List<Long> postid_list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
-        act_title=new String[1000];
-        act_sender=new String[1000];
-        postid=new Long[1000];
-        act_address=new String[1000];
-        act_context=new String[1000];
-        act_date=new Date[1000];
-        title=findViewById(R.id.title);
-        sender=findViewById(R.id.sender);
-        address=findViewById(R.id.address);
-        context=findViewById(R.id.sender);
-        date=findViewById(R.id.date);
+        act_title = new String[1000];
+        act_sender = new String[1000];
+        postid = new Long[1000];
+        act_address = new String[1000];
+        act_context = new String[1000];
+        act_date = new Date[1000];
+        title = findViewById(R.id.title);
+        sender = findViewById(R.id.sender);
+        address = findViewById(R.id.address);
+        context = findViewById(R.id.sender);
+        date = findViewById(R.id.date);
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         db.collection("active")
                 .get()
@@ -46,34 +50,35 @@ public class fragment extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
-                           int i=0;
-                           long x;
+                            int i = 0;
+                            long x;
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 // Log.d("HII", i+":"+document.getId() + " => " + document.getString("petname"));
-                                act_title[i]=document.getString("title");
-                                postid[i]=document.getLong("postid");
-                                act_address[i]=document.getString("address");
-                                act_context[i]=document.getString("detail");
-                                act_date[i]=document.getDate("posttime");
-                                x=document.getLong("actid");
+                                act_title[i] = document.getString("title");
+                                postid_list.add(document.getLong("postid"));
+//                                postid[i]=document.getLong("postid");
+                                act_address[i] = document.getString("address");
+                                act_context[i] = document.getString("detail");
+                                act_date[i] = document.getDate("posttime");
+                                x = document.getLong("actid");
                                 SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
-                                String newdate = sdf.format(act_date[i]);
+                                String newdate = DateFormat.getInstance().format(act_date[i]);
                                 //act_context[i].substring(3,8);
-                                act_context[i]=act_context[i].substring(0,15)+"....";
-                                Log.d("22",i+":"+ x);
-                                for(int z=0;z<=com.example.myapplication.login.i;z++){
-                                    if(postid[i]==com.example.myapplication.login.uid[z]){
-                                        act_sender[i]=com.example.myapplication.login.name[z];
+                                act_context[i] = act_context[i].substring(0, 15) + "....";
+                                Log.d("22", i + ":" + x);
+                                for (int z = 0; z <= com.example.myapplication.login.i; z++) {
+                                    if (postid[i] == com.example.myapplication.login.uid[z]) {
+                                        act_sender[i] = com.example.myapplication.login.name[z];
                                     }
                                 }
 
                                 title.setText(act_title[i]);
-                                sender.setText("發佈人:"+act_sender[i]);
-                                address.setText("地點:"+act_address[i]);
+                                sender.setText("發佈人:" + act_sender[i]);
+                                address.setText("地點:" + act_address[i]);
                                 context.setText(act_context[i]);
                                 date.setText(newdate);
 
-                               // Log.d("22",i+":"+user_pwd[i]);
+                                // Log.d("22",i+":"+user_pwd[i]);
                                 i++;
                             }
                         } else {
@@ -83,22 +88,20 @@ public class fragment extends AppCompatActivity {
                 });
 
 
-
-
-        Button pick= findViewById(R.id.send);
-        pick.setOnClickListener(new View.OnClickListener(){
+        Button pick = findViewById(R.id.send);
+        pick.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 Intent change = new Intent(fragment.this, release.class);
                 startActivity(change);
             }
 
         });
 
-        Button per= findViewById(R.id.personal);
-        per.setOnClickListener(new View.OnClickListener(){
+        Button per = findViewById(R.id.personal);
+        per.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 Intent change = new Intent(fragment.this, person.class);
                 startActivity(change);
             }
@@ -106,20 +109,20 @@ public class fragment extends AppCompatActivity {
         });
 
 
-        Button imf= findViewById(R.id.act1);
-        imf.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent change = new Intent(fragment.this, cancel.class);
-                startActivity(change);
-            }
+//        Button imf= findViewById(R.id.act1);
+//        imf.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view){
+//                Intent change = new Intent(fragment.this, cancel.class);
+//                startActivity(change);
+//            }
+//
+//        });
 
-        });
-
-        Button home= findViewById(R.id.back);
-        home.setOnClickListener(new View.OnClickListener(){
+        Button home = findViewById(R.id.back);
+        home.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 Intent change = new Intent(fragment.this, photo.class);
                 startActivity(change);
             }
--- app/src/main/java/com/example/myapplication/login.java
+++ app/src/main/java/com/example/myapplication/login.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import io.opencensus.internal.StringUtils;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -28,23 +29,24 @@ public class login extends AppCompatActivity implements View.OnClickListener {
     EditText id;
     EditText pwd;
     TextView yesnull;
-    static String[] user_email,user_pwd,name;
+    static String[] user_email, user_pwd, name;
     static Long[] uid;
     static Long loginid;
     static int i; //資料庫長度
     String userid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        user_email=new String[1000];
-        user_pwd=new String[1000];
-        uid=new Long[1000];
-        name=new String[1000];
+        user_email = new String[1000];
+        user_pwd = new String[1000];
+        uid = new Long[1000];
+        name = new String[1000];
         TextView bb = findViewById(R.id.forget);
-        bb.setOnClickListener(new View.OnClickListener(){
+        bb.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 Intent change = new Intent(login.this, tryy.class);
                 startActivity(change);
             }
@@ -52,8 +54,8 @@ public class login extends AppCompatActivity implements View.OnClickListener {
         });
 
         /*忘記密碼，未寫完!*/
-        yesnull=(TextView)findViewById(R.id.yesnull);
-        yesnull.setVisibility(View.GONE);
+        yesnull = (TextView) findViewById(R.id.yesnull);
+        yesnull.setText("");
         id = (EditText) findViewById(R.id.id);
         pwd = (EditText) findViewById(R.id.pwd);
         /*下面這段在寫登入，尚未連結資料庫，因此寫死*/
@@ -64,15 +66,15 @@ public class login extends AppCompatActivity implements View.OnClickListener {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
-                            i=0;
+                            i = 0;
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 // Log.d("HII", i+":"+document.getId() + " => " + document.getString("petname"));
-                                user_email[i]=document.getString("email");
-                                user_pwd[i]=document.getString("pwd");
-                                uid[i]=document.getLong("uid");
-                                name[i]=document.getString("name");
-                                Log.d("22",i+":"+user_email[i]);
-                                Log.d("22",i+":"+user_pwd[i]);
+                                user_email[i] = document.getString("email");
+                                user_pwd[i] = document.getString("pwd");
+                                uid[i] = document.getLong("uid");
+                                name[i] = document.getString("name");
+                                Log.d("22", i + ":" + user_email[i]);
+                                Log.d("22", i + ":" + user_pwd[i]);
                                 i++;
                             }
                         } else {
@@ -91,22 +93,21 @@ public class login extends AppCompatActivity implements View.OnClickListener {
 
         switch (v.getId()) {
             case R.id.login:
-                int right=0;
-                if(id.getText().toString().equals("") || pwd.getText().toString().equals("")){
-                        yesnull.setText("帳號或密碼不可為空");
-                        yesnull.setVisibility(View.VISIBLE);
-                }else{
-                    for(int x=0;x<=i;x++){
-                        if(id.getText().toString().equals(user_email[x]) && pwd.getText().toString().equals(user_pwd[x])){
-                            right=1;
-                            Intent pick=new Intent(login.this,photo.class);
-                            userid=id.getText().toString();
-                            loginid=uid[x];
+                int right = 0;
+                if (id.getText().toString().isEmpty() || pwd.getText().toString().isEmpty()) {
+                    yesnull.setText(getResources().getString(R.string.input_tip));
+                } else {
+                    for (int x = 0; x <= i; x++) {
+                        if (id.getText().toString().equals(user_email[x]) && pwd.getText().toString().equals(user_pwd[x])) {
+                            right = 1;
+                            Intent pick = new Intent(login.this, photo.class);
+                            userid = id.getText().toString();
+                            loginid = uid[x];
                             startActivity(pick);
                         }
                     }
-                    if(right!=1){
-                        Intent pick=new Intent(login.this,tryy.class);
+                    if (right != 1) {
+                        Intent pick = new Intent(login.this, tryy.class);
                         startActivity(pick);
                     }
                 }
